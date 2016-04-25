e292f56faea3970a66386874f54065bbdb97909a
Nevernown <ca.boel@student.avans.nl>
2016-04-25 16:46:35 +0200

    test... kinda

---
+++ test/GameListController.spec.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Nevernown on 25-4-2016.
+ */
+
+describe("GameListController", function () {
+    
+    var gameFactory;
+    var scope;
+    var gameListController;
+    
+    beforeEach(function () {
+        module("mahjong");
+        
+        
+        inject(function ($rootScope, $controller, $injector) {
+            scope = $rootScope.$new();
+            gameListController = $controller
+        })
+    });
+    
+    describe("getGames", function () {
+        it("should return 2 games", function () {
+            
+        });
+    });
+});
