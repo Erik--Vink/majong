893643de4eda9e79368dd757b1e29613093303dd
Nevernown <ca.boel@student.avans.nl>
2016-06-07 10:40:55 +0200

    Underscore Madness

--- app/js/components/board/board.controller.js
+++ app/js/components/board/board.controller.js
@@ -1,6 +1,47 @@
 /**
  * Created by Nevernown on 6-6-2016.
  */
-module.exports = function(GameService, AuthFactory, /* MatchService, */ $scope){
+var _ = require("underscore");
 
+module.exports = function(MatchFactory, $scope){
+    var self = this;
+
+    self.board = $scope.tiles;
+
+    self.SelectTile = function (tile) {
+        if(self.canSelect(tile))
+        {
+            MatchFactory.AddTile(tile);
+        }
+    };
+
+    self.canSelect = function (tile) {
+        return (!self.topBlocked(tile) && (!self.leftBlocked(tile) || self.rightBlocked(tile)));
+    };
+
+    self.topBlocked = function (tile) {
+        var layer = tile.zPos + 1;
+        var xRange = [tile.xPos - 1, tile.xPos, tile.xPos + 1];
+        var yRange = [tile.yPos - 1, tile.yPos, tile.yPos + 1];
+
+        var surface = _.flatten(_.map(xRange, function (x) {
+            return _.map(yRange, function (y) {
+               return {x: x, y: y};
+            });
+        }));
+
+        return _.any(self.board, function (tile) {
+                _.any(surface, function (pos) {
+                    return (tile.zPos == layer && tile.xPos == pos.x && tile.yPos == poz.z);
+                })
+            });
+    };
+
+    self.leftBlocked = function (tile) {
+
+    };
+
+    self.rightBlocked = function (tile) {
+
+    };
 };
