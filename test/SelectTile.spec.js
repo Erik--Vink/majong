6ef16720334954c08ff29402af9b6d93ff2d5099
Nevernown <ca.boel@student.avans.nl>
2016-06-13 14:12:10 +0200

    test setup... kinda

---
+++ test/SelectTile.spec.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Nevernown on 13-6-2016.
+ */
+describe("Selecting Tiles", function() {
+    var BoardController;
+
+    beforeEach(module('app'));
+    beforeEach(module('../app/partials/game-board.html'));
+
+    beforeEach(inject(function(_BoardController_){
+        BoardController = _BoardController_;
+    }));
+
+    it('should render the person directive', function() {
+
+    });
+});
