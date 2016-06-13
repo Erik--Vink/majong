4690d744a0d9e94ebbb6e849aa32ea26a3df5b35
erik--vink <erik.vink95@gmail.com>
2016-06-13 13:09:43 +0200

    Visual glitch

--- app/js/components/tile/tile.directive.js
+++ app/js/components/tile/tile.directive.js
@@ -20,7 +20,7 @@ module.exports = function(){
             var tileDiv = $(element).find(".tile");
             tileDiv.css("left", scope.tile.xPos * 34 + (scope.tile.zPos * 4));
             tileDiv.css("top", scope.tile.yPos * 44 - (scope.tile.zPos * 4));
-            var zIndex = 100 + scope.tile.zPos *10 + (((scope.tile.yPos % 2 == 0)?scope.tile.yPos:scope.tile.yPos+1) + (scope.tile.zPos * scope.tile.yPos) - (scope.tile.xPos));
+            var zIndex = 100 + scope.tile.zPos *10 + (((scope.tile.yPos % 2 == 0)?scope.tile.yPos:scope.tile.yPos+1) + (scope.tile.zPos * scope.tile.yPos) - (scope.tile.xPos * 2));
             tileDiv.css("z-index", zIndex);
         }
     };
